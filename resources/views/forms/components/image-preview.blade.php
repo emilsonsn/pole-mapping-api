5c9d1f9fa320bb722d951bfbccdbe5dbf9b7a7ec
Emilson de Souza Nascimento <emilsonsn2@gmail.com>
2025-09-16 10:10:02 -0300

    fix: corrige imagem

--- resources/views/forms/components/image-preview.blade.php
+++ resources/views/forms/components/image-preview.blade.php
@@ -1,5 +1,5 @@
 @if ($getRecord() && $getRecord()->photo_path)
-    <img src="{{ asset('storage/' . $getRecord()->photo_path) }}"
+    <img src="{{  $getRecord()->photo_path }}"
          alt="Foto da manutenção"
          style="max-height:200px; border-radius:8px;">
 @else
